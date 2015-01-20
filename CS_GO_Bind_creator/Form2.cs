10b322da0666c55761dedf3040aa18f1438b5283
MisterX2000 <MisterX2000XD@gmail.com>
2015-01-20 14:13:00 +0100

    Kleinen fehler behoben

---
+++ CS_GO_Bind_creator/Form2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS_GO_Bind_creator
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+      private void Form2_Load(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.english == true)
+            {
+                rb_english.Checked = true;
+                rb_german.Checked = false;
+                this.Text = "Options";
+                gb_language.Text = "Language";
+                gb_keyboard.Text = "Keyboard";
+                rb_english.Text = "English";
+                rb_german.Text = "German";
+                lb_warning.Text = "--- Restart on change ---";
+            }
+            else
+            {
+                rb_german.Checked = true;
+                rb_english.Checked = false;
+                this.Text = "Einstellungen";
+                gb_language.Text = "Sprache";
+                gb_keyboard.Text = "Tastatur";
+                rb_english.Text = "Englisch";
+                rb_german.Text = "Deutsch";
+                lb_warning.Text = "--- Bei änderung Neustarten ---";
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default["english"] = true;
+            Properties.Settings.Default.Save();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default["english"] = false;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
